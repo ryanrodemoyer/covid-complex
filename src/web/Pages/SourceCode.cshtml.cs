94e9313f4e9b275b6769f3111c257f16590efd4e
Ryan Rodemoyer <ryan.rodemoyer@icloud.com>
2020-12-12 11:53:33 -0700

    SourceCode: created new page to link to repo

---
+++ src/web/Pages/SourceCode.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace web.Pages
+{
+    public class SourceCodeModel : PageModel
+    {
+        private readonly ILogger<SourceCodeModel> _logger;
+
+        public SourceCodeModel(
+            ILogger<SourceCodeModel> logger
+        )
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        //public string FlashMessage { get; set; }
+
+        //public IActionResult OnPost()
+        //{
+        //    return new PageResult();
+        //}
+    }
+}
