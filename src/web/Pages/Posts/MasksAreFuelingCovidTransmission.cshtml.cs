63fa32ac4e425f054505562f2f369b1c837cbeb5
Ryan Rodemoyer <ryan.rodemoyer@icloud.com>
2020-12-12 11:54:25 -0700

    Post: masks are fueling transmission

---
+++ src/web/Pages/Posts/MasksAreFuelingCovidTransmission.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace web.Pages
+{
+    public class MasksAreFuelingCovidTransmissionModel : PageModel
+    {
+        private readonly ILogger<MasksAreFuelingCovidTransmissionModel> _logger;
+
+        public MasksAreFuelingCovidTransmissionModel(
+            ILogger<MasksAreFuelingCovidTransmissionModel> logger
+        )
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        //public string FlashMessage { get; set; }
+
+        //public IActionResult OnPost()
+        //{
+        //    return new PageResult();
+        //}
+    }
+}
